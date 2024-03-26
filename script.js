d6687bf3b89f6dc659649c3691ba9c4736ab2eae
Anastasia Bobere <anastasia.bobere@gmail.com>
2024-03-26 21:48:25 +0200

    help

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//menu function
+// menu function
 document.addEventListener("DOMContentLoaded", function () {
   const menuButton = document.querySelector(".menu");
   const header = document.getElementById("nav-con");
@@ -12,27 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
     header.classList.remove("open");
   });
 });
+// login?
+// const express = require("express");
+// const app = express();
 
-//reveal function
-// const items = document.querySelector(".about-div");
-
-// const isInViewport = (el) => {
-//   const rect = el.getBoundingClientRect();
-//   return (
-//     rect.top >= 0 &&
-//     rect.left >= 0 &&
-//     rect.bottom <=
-//       (window.innerHeight || document.documentElement.clientHeight) &&
-//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// };
-// const run = () =>
-//   items.forEach((item) => {
-//     if (isInViewport(item)) {
-//       item.classList.add("show");
-//     }
-//   });
-// //Events
-// window.addEventListener("load", run);
-// window.addEventListener("resize", run);
-// window.addEventListener("scroll", run);
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello</h1>");
+// });
+// app.listen(5501, () => {
+//   console.log("server PUSSSYYY  ");
+// });
